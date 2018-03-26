806636778825a1783c31b7207f9ade72943aae28
RC <13718988038@sina.cn>
2018-03-26 02:35:19 -0400

    Second

--- public/js/app.js
+++ public/js/app.js
@@ -5,34 +5,47 @@ var app;
 var cursorDefaultColor = '#40d58e';
 var cursorEventColor = '#9bffec';
 
+
 $(function () {
 
 
     app = new Vue({
         el      : '#app',
         data    : {
-            platform       : '0',
-            leapStatus     : false,
-            socketStatus   : false,
-            message        : 'Hello Vue!',
-            frameData      : '0',
-            pointableString: '0',
-            trackingStatus: false,
-            cursorPosString: '0',
-            cursorPosArray : [],
-            cursorLevel    : 0, //3-overview. 2-browsing. 1-action. 0-out of range
-            cursorName     : '',
-            currentFolder  : {
-                name: 'Daily',
-                translatePos: [-64,38]
+            platform        : '0',
+            baseScale: [1.8,2.2],
+            leapStatus      : false,
+            socketStatus    : false,
+            message         : 'Hello Vue!',
+            frameData       : '0',
+            pointableString : '0',
+            trackingStatus  : false,
+            cursorPosString : '0',
+            cursorPosArray  : [],
+            cursorHoverCount: 0,
+            cursorLevel     : 0, //3-overview. 2-browsing. 1-action. 0-out of range
+            cursorName      : '',
+            currentFolder   : {
+                name        : 'Daily',
+                translatePos: [-64, 38]
             },
-            appInfo        : {
+            currentButton   : {
+
+                name        : 'None',
+                trigger: false,
+                last :'None'
+            },
+            currentIcon:{
+                name:'None',
+                num:0
+            },
+            appInfo         : {
                 currentGesturef: '',
-                lastGesture   : '',
+                lastGesture    : '',
 
 
             },
-            appControl     : {
+            appControl      : {
                 scalePicked: 1,
                 viewPicked : 'slide'
             }
@@ -42,8 +55,13 @@ $(function () {
         computed: {
             currentScale : function () {
                 var p = parseInt(this.appControl.scalePicked);
-                var sx = 1.6;
-                var sy = 2.2;
+
+               /*
+               Coordinate Scale
+               */
+                var sx = this.baseScale[0];
+                var sy = this.baseScale[1];
+
                 if (p === 1) {
                     sx *= 1;
                     sy *= 1;
@@ -80,9 +98,39 @@ $(function () {
                     }
 
                 }
-                else {
-                    flag = false;
+                else if (parseInt(this.cursorLevel) === 2) {
+                    this.currentIcon=getIconByPos(this.cursorPosArray);
+                    if (this.currentIcon.name !== 'None') {
+                        setCursorColor(cursorEventColor);
+
+
+                        flag = true;
+                    }
+                    else {
+                        flag = false;
+                    }
+
                 }
+
+
+                else if (parseInt(this.cursorLevel) === 1) {
+
+                    this.currentButton=getButtonByPos(this.cursorPosArray);
+                    if(this.currentButton.trigger===true){
+                        var id='.screen-btn.'+this.currentButton.name+'-btn'
+
+                        //alert(id);
+
+                        buttonAnime(id);
+                        if(this.currentButton.name==='middle'){
+                            showAppPopup();
+                        }
+
+                    }
+                }
+
+
+
                 if (!flag) {
 
                     setCursorColor(cursorDefaultColor);
@@ -97,13 +145,14 @@ $(function () {
                 var brScale = anime();
                 var ovScale = anime();
 
-                var transX=this.currentFolder.translatePos[0];
-                var transY=this.currentFolder.translatePos[1];
+                var transX = this.currentFolder.translatePos[0];
+                var transY = this.currentFolder.translatePos[1];
                 if (val === 0) {
 
                     showView('#lock-screen');
                 }
                 if (val === 3) {
+                    this.baseScale=[1.4,1.8];
 
 
                     ovScale = anime({
@@ -127,6 +176,8 @@ $(function () {
                     showView('#overview-level');
                 }
                 if (val === 1) {
+                    this.baseScale=[2.6,3.0];
+
                     brScale = anime({
                         targets   : '#browsing-level',
                         translateX: 0,
@@ -145,8 +196,10 @@ $(function () {
                     showView('#action-level');
                 }
                 if (val === 2) {
+                    this.baseScale=[1.8,2.2];
+
                     $('#browsing-level').css({
-                        transform: 'translate('+transX+'px,'+transY+'px)'
+                        transform: 'translate(' + transX + 'px,' + transY + 'px)'
                     })
                     ovScale = anime({
                         targets : '#overview-level',
@@ -165,7 +218,7 @@ $(function () {
                         delay     : 200
                     });
                     showSlideBtns(0);
-
+                    hideLoadingCursor();
                     hideViews();
 
                 }
@@ -181,6 +234,8 @@ $(function () {
     var controllerOptions = {enableGestures: true};
     var frameString = '';
     var pointableString = 'Pointable';
+
+
     if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
         app.platform = 'Iphone'
     } else {
@@ -246,82 +301,6 @@ $(function () {
     }
 
 
-    function getFolderByPos(a) {
-        var rad = 40;
-        var posDaily = [92, 197];
-        var posMedia = [159, 76];
-        var posSocial = [217, 197];
-        var name;
-        var tPos=[];
-        if (inCircleArea(a, posDaily, rad)) {
-            slideTo(0);
-            name= 'Daily';
-            tPos=[-64,42];
-        }
-        else if (inCircleArea(a, posMedia, rad)) {
-            slideTo(1);
-            name= 'Media';
-            tPos=[0,-78];
-        }
-        else if (inCircleArea(a, posSocial, rad)) {
-            slideTo(2);
-            name= 'Social';
-            tPos=[63,42];
-        }
-        else {
-            slideTo(3);
-            name= 'None';
-            tPos=[0,0];
-        }
-        return {
-            name:name,
-            translatePos: tPos
-        }
-
-    }
-
-    function inCircleArea(point, circle, r) {
-        var x1 = point[0];
-        var y1 = point[1];
-        var x2 = circle[0];
-        var y2 = circle[1];
-
-        //console.log(Math.sqrt(((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))));
-        return ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < r * r;
-
-
-    }
-
-    function showView(id) {
-
-        var otherLayers = $('.layer:not(' + id + ')')
-        otherLayers.css({
-            opacity: 0
-
-        });
-
-        setTimeout(function () {
-            otherLayers.css({
-                zIndex: 0
-            })
-
-        }, 0)
-        $(id).css({
-            zIndex : 1,
-            opacity: 1
-        })
-
-
-    }
-
-    function hideViews() {
-        $('.layer').css({
-            opacity: 0
-
-        });
-    }
-
-
     function drawCursor(id, c, emit) {
         var cursor = $(id);
         var xpos = parseInt(c.x);
@@ -334,11 +313,11 @@ $(function () {
                 });
 
                 if (c.out) {
-                    app.trackingStatus=false;
+                    app.trackingStatus = false;
                     setContainerColor('#d2737ca3');
                 }
                 else {
-                    app.trackingStatus=true;
+                    app.trackingStatus = true;
                     setContainerColor('#1f242959');
                 }
 
@@ -355,12 +334,12 @@ $(function () {
         var xpos = parseInt(c.x);
         var ypos = parseInt(c.y);
         anime({
-            targets   : id,
-            left: xpos + 'px',
-            top : ypos + 'px'
+            targets: id,
+            //left: xpos + 'px',
+            top    : ypos + 'px'
         });
         cursor.find('.cursorPoint').css({
-            background: c.color
+            // background: c.color
         });
     }
 
@@ -388,8 +367,38 @@ $(function () {
 
 function slideTo(n) {
     var mySwiper = document.querySelector('.swiper-container').swiper
-    var a = mySwiper.slideTo(n,false);
+    var a = mySwiper.slideTo(n, false);
     return a;
+}
+
+
+function showView(id) {
+
+    var otherLayers = $('.layer:not(' + id + ')')
+    otherLayers.css({
+        opacity: 0
+
+    });
+
+    setTimeout(function () {
+        otherLayers.css({
+            zIndex: 0
+        })
+
+    }, 0)
+    $(id).css({
+        zIndex : 1,
+        opacity: 1
+    })
+
+
+}
+
+function hideViews() {
+    $('.layer').css({
+        opacity: 0
+
+    });
 }
 
 
@@ -412,10 +421,10 @@ function getWatchPosByLeap(x, y, z) {
     var y1 = r + y * app.currentScale.y;
 
     // level height
-    var zBottomPos = 30;
-    var z1Pos = 100;
-    var z2Pos = 170;
-    var zTopPos = 280;
+    var zBottomPos = 10;
+    var z1Pos = 80;
+    var z2Pos = 160;
+    var zTopPos = 300;
 
 
     var c = cursorDefaultColor;
@@ -487,6 +496,223 @@ function getLevelName(lvl) {
 }
 
 
+function getButtonByPos(a) {
+    var len = 50;
+    var count = app.cursorHoverCount;
+    var rad = 90;
+    var offset=30;
+    var screenW=325;
+    var cursorW=32;
+    var left = [-offset, screenW/2];
+    var right = [screenW+offset-cursorW,screenW/2];
+    var top = [screenW/2, -offset];
+    var bottom = [screenW/2, screenW+offset-cursorW];
+    var middle = [screenW/2-cursorW/2,screenW/2-cursorW/2];
+
+    var name;
+    var last=app.currentButton.last;
+    var trigger = false;
+    if (inCircleArea(a, left, rad)) {
+        name = 'left';
+    }
+    else if (inCircleArea(a, top, rad)) {
+        name = 'top';
+    }
+    else if (inCircleArea(a, right, rad)) {
+        name = 'right';
+    }
+    else if (inCircleArea(a, bottom, rad)) {
+        name = 'bottom';
+    }
+    else if (inCircleArea(a, middle, rad-40)) {
+        name = 'middle';
+    }
+    else {
+        name = 'None';
+    }
+
+
+    if (name === 'None') {
+        app.cursorHoverCount = 0;
+        last='None';
+    }
+    else {
+        if (count < len && last==='None') {
+
+            app.cursorHoverCount++;
+        }
+        else if (count === len) {
+            app.cursorHoverCount=0;
+            trigger=true;
+            last=name;
+
+
+        }
+    }
+
+    var loadingH=map_range(count, 0,len,0,100);
+    $('.cursorLoadingPoint').css({
+        height: loadingH + '%'
+    });
+
+    return {
+        name: name,
+        trigger: trigger,
+        last: last
+    }
+}
+
+
+function buttonAnime(id) {
+    //var originColor=$(id).css('background');
+    var originColor='#667890';
+    if(id.includes('middle')){
+        var color = anime({
+            targets: id,
+
+            translateX: '+=0',
+            translateY: '+=0',
+            scale:[
+                {value: '+=0.5'},
+                {value: '-=0.5'}
+            ],
+            backgroundColor: [
+                {value: '#40d58e'}, // Or #FFFFFF
+                {value: '#63ffa6'}
+            ],
+            opacity:[
+                {value: '0.5'},
+                {value: '0'}
+            ],
+            easing: 'linear',
+            direction: 'alternate',
+            duration: 600
+        });
+    }
+    else{
+        var colors = anime({
+            targets: id,
+
+            translateX: '+=0',
+            translateY: '+=0',
+            scale:[
+                {value: '+=0.1'},
+                {value: '-=0.1'}
+            ],
+            backgroundColor: [
+                {value: '#40d58e'},
+                {value: originColor}
+            ],
+            easing: 'linear',
+            direction: 'alternate',
+            duration: 600
+        });
+    }
+
+    hideLoadingCursor();
+
+}
+
+function hideLoadingCursor() {
+    $('.cursorLoadingPoint').css({
+        height:0
+    });
+}
+
+function getFolderByPos(a) {
+    var rad = 55;
+    var posDaily = [92, 197];
+    var posMedia = [159, 76];
+    var posSocial = [217, 197];
+    var name;
+    var tPos = [];
+    if (inCircleArea(a, posDaily, rad)) {
+        slideTo(0);
+        name = 'Daily';
+        tPos = [-64, 42];
+    }
+    else if (inCircleArea(a, posMedia, rad)) {
+        slideTo(1);
+        name = 'Media';
+        tPos = [0, -78];
+    }
+    else if (inCircleArea(a, posSocial, rad)) {
+        slideTo(2);
+        name = 'Social';
+        tPos = [63, 42];
+    }
+    else {
+        slideTo(3);
+        name = 'None';
+        tPos = [0, 0];
+    }
+    return {
+        name        : name,
+        translatePos: tPos
+    }
+
+}
+
+function getIconByPos(a) {
+    var rad = 40;
+    var pos1 = [134, 161];
+    var pos2 = [63, 161];
+    var pos3 = [95, 231];
+    var pos4 = [173, 232];
+    var pos5 = [211, 162];
+    var pos6 = [239, 224];
+    var name,num;
+    if(app.currentFolder.name==='Daily') {
+        if (inCircleArea(a, pos1, rad)) {
+            num = 1;
+            name = 'Weather';
+        }
+        else if (inCircleArea(a, pos2, rad)) {
+            num = 2;
+            name = 'Stocks';
+        }
+        else if (inCircleArea(a, pos3, rad)) {
+            num = 3;
+            name = 'Activity';
+        }
+        else if (inCircleArea(a, pos4, rad)) {
+            num = 4;
+            name = 'To-do List';
+        }
+        else if (inCircleArea(a, pos5, rad)) {
+            num = 5;
+            name = 'Alarm';
+        }
+        else if (inCircleArea(a, pos6, rad)) {
+            num = 6;
+            name = 'Watch Pay';
+        }
+        else {
+            num = 0;
+            name = 'None';
+        }
+    }
+    else {
+        num = 0;
+        name = 'None';
+    }
+
+    return {
+        name        : name,
+        num: num
+    }
+
+}
+
+function showAppPopup() {
+    anime({
+        targets: '#popup-screen',
+        opacity: 1,
+        scale: 1,
+        easing: 'easeInOutQuad'
+    });
+}
+
 function showSlideBtns(v) {
 
     var o = map_range(v, 0, 40, 0, 1);
@@ -548,4 +774,44 @@ function vectorToString(vector, digits) {
 
 function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+function inCircleArea(point, circle, r) {
+    var x1 = point[0];
+    var y1 = point[1];
+    var x2 = circle[0];
+    var y2 = circle[1];
+
+    //console.log(Math.sqrt(((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))));
+    return ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < r * r;
+
+
+}
+
+function Stack() {
+    var items = [];
+    this.push = function (element) {
+        items.push(element);
+    };
+    this.pop = function () {
+        return items.pop();
+    };
+    this.peek = function () {
+        return items[items.length - 1];
+    };
+    this.isEmpty = function () {
+        return items.length == 0;
+    };
+    this.size = function () {
+        return items.length;
+    };
+    this.clear = function () {
+        items = [];
+    };
+    this.print = function () {
+        console.log(items.toString());
+    };
+    this.toString = function () {
+        return items.toString();
+    };
 }
